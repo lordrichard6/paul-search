361bc848fca148847f173b53f6bfcd8d114dd14f
lordrichard6 <paulolopesreizinho@gmail.com>
2021-04-15 22:37:53 +0200

    start search page

---
+++ pages/search.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+function Search() {
+  return (
+      <div>
+          <Head>
+              <title>Search Results</title>
+              <link rel="icon" href="/favicon.ico"/>
+          </Head>
+          {/* Header */}
+          <h1>Search page</h1>
+          {/* Search Results */}
+      </div>
+  )
+}
+
+export default Search;
