1001cacfc0aaa38b849766cd08d12a252645020c
lordrichard6 <paulolopesreizinho@gmail.com>
2021-04-15 21:06:11 +0200

    search functionality

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 import { SearchIcon } from "@heroicons/react/outline";
 import Image from "next/image";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 import Head from "next/head";
 import Avatar from "../components/Avatar";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`search?term=${term}`)
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -39,12 +53,20 @@ export default function Home() {
         />
         <div className="flex w-full hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
           <SearchIcon className="h-5 mr-3 text-gray-500" />
-          <input type="text" className="focus:outline-none flex-grow" />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="focus:outline-none flex-grow"
+          />
           <MicrophoneIcon className="h-5 mr-3 text-gray-500" />
         </div>
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-          <button className="btn">Paul Search</button>
-          <button className="btn">Try Something</button>
+          <button onClick={search} className="btn">
+            Paul Search
+          </button>
+          <button onClick={search} className="btn">
+            Try Something
+          </button>
         </div>
       </form>
       {/* Footer */}
