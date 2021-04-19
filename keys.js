8c7af11433b80a17c1fade1859fc9ab6134b11ca
lordrichard6 <paulolopesreizinho@gmail.com>
2021-04-19 09:39:07 +0200

    api keys restore(vul)

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = ' AIzaSyA7zSCRgRqentRqqEdvb4KMwGR-8DerfUE';
+
+// Contex Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = 'f33f295d20bd74724';
--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import Header from "../components/Header";
 import SearchResults from "../components/SearchResults";
 import Response from "../Response";
+import { API_KEY, CONTEXT_KEY } from '../keys';
 
 function Search({ results }) {
   const router = useRouter();
@@ -29,14 +30,14 @@ export async function getServerSideProps(context) {
   const useDummyData = false;
   const startIndex = context.query.start || "0";
 
-  const API_KEY = process.env.API_KEY
-  const CONTEXT_KEY = process.env.CONTEXT_KEY
+  // const API_KEY = process.env.API_KEY
+  // const CONTEXT_KEY = process.env.CONTEXT_KEY
 
   const data = useDummyData
     ? Response
     : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
-      ).then((res) => res.json());
+      `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+    ).then((res) => res.json());
 
   // After the SERVER has rendered, pass the results to the client.
   return {
